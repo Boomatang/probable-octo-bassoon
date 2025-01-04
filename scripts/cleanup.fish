500486102cd82af76f56df9722b30788b697da9e
Jim Fitzpatrick <jfitzpat@redhat.com>
2025-01-04 12:13:47 +0000

    ADD: clean up script
    For when things go wrong, and you need to clean stuff up manually.
    
    Signed-off-by: Jim Fitzpatrick <jfitzpat@redhat.com>

---
+++ scripts/cleanup.fish
@@ -0,0 +1,24 @@
+#!/usr/bin/env fish
+
+if test (count $argv) -lt 1
+	echo "Usage: cleanup <namespace>"
+	return 1
+end
+
+set namespace $argv[1]
+set resources "ratelimitpolicy" "gateway" "httproute"
+
+for resource in $resources
+	# Attempt to delete resources
+	echo "Deleting all $resource resources in the $namespace namespace..."
+	kubectl -n $namespace delete $resource --all
+
+	# Check the exit status
+	if test $status -eq 0
+		echo "Successfully deleted all $resource resources in the $namespace namespace."
+	else
+		echo "Failed to delete resources. Please check for errors."
+	end
+	echo
+end
+
